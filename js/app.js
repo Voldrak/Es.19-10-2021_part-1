a61c1778b2509fbc763b325a84fc3fb4af31751e
Marco Messina <voldrak94@gmail.com>
2021-10-19 16:24:42 +0200

    esercizio 1 completato

--- js/app.js
+++ js/app.js
@@ -1,7 +1,15 @@
-let isAnimal = Dog;
-let isAnimal = Cat;
-let isAnimal = Goldfish;
-let isAnimal = Monkey;
-let isAnimal = Panda;
+let isAnimal = prompt("Animale preferito:\n 1. Dog\n 2. Cat\n 3. Goldfish\n 4. Monkey\n 5. Panda ");
 
-prompt(Qual è il tuo animale preferito?)
+if (isAnimal == 1) {
+    console.log("Chose the Dog");
+} else if (isAnimal == 2) {
+    console.log("Chose the Cat");
+} else if (isAnimal == 3) {
+    console.log("Chose the Goldfish");
+} else if (isAnimal == 4) {
+    console.log("Chose the Monkey");
+} else if (isAnimal == 5) {
+    console.log("Chose the Panda");
+} else if (isAnimal > 5) {
+    console.log("Animal not found");
+}
