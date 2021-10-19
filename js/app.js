69c755aee53f893ad37edc2766d018cc1bf1596c
Marco Messina <voldrak94@gmail.com>
2021-10-19 15:45:07 +0200

    inizio esercizio parte 1

--- js/app.js
+++ js/app.js
@@ -1,32 +1,7 @@
-let age = "16";
-let tot = "99";
+let isAnimal = Dog;
+let isAnimal = Cat;
+let isAnimal = Goldfish;
+let isAnimal = Monkey;
+let isAnimal = Panda;
 
-let totale = age > 15 ? "Marco" : ( age < 9 ? "Mario" : "Rossi");
-
-console.log(totale);
-
-/* if( age > 15 ) {
-    let totale = "Marco";
-} else {
-    let totale = "Messina";
-} */
-
-switch (age) {
-    case 17:
-
-    alert("minore di 18");
-
-    break;
-
-    case 19:
-        
-    alert("maggiore di 18");
-
-    break;
-
-    default:
-
-    alert("Sei maggiorenne");
-
-    break;
-}
+prompt(Qual è il tuo animale preferito?)
