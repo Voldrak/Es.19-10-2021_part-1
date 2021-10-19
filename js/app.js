262a16903089ce3f752cc37b43bcc3ec3b5ddbdc
Marco Messina <voldrak94@gmail.com>
2021-10-19 12:07:31 +0200

    esempi vari

--- js/app.js
+++ js/app.js
@@ -1,3 +1,15 @@
-let age = "27";
+let myName = "Marco";
+
+let mySurname = 'Messina';
+
+let job = `<div>
+                <div>
+                    <p> pippo </p>
+                </div>
+            </div>`
+
+let age = 27;
+
+let isDark = false;
 
 console.log(age);
